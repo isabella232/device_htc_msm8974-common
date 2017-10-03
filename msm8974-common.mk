c227fab2577f0cb1dcc43b560f89a5dc1ac56f55
Bruno Martins <bgcngm@gmail.com>
2017-10-03 16:15:03 +0100

    msm8974-common: Clean up OMX codecs packages
     * DivX DRM is dead
     * Remove other leftovers
    
    Change-Id: Ie9cb51144e27350a686ef71f23899fc0aabe4ee0

--- msm8974-common.mk
+++ msm8974-common.mk
@@ -188,8 +188,6 @@ PRODUCT_PACKAGES += \
 # OMX
 PRODUCT_PACKAGES += \
     libc2dcolorconvert \
-    libdivxdrmdecrypt \
-    libextmedia_jni \
     libOmxAacEnc \
     libOmxAmrEnc \
     libOmxCore \
@@ -197,7 +195,6 @@ PRODUCT_PACKAGES += \
     libOmxQcelp13Enc \
     libOmxVdec \
     libOmxVenc \
-    libOmxVidcCommon \
     libstagefrighthw
 
 # Power
